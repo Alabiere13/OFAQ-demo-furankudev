1a2fa03701fc3fe2bc83deeb393cc3e14ad7bc35
Live session user <furanku13code@gmail.com>
2019-03-05 16:45:49 +0100

    routes QuestionController

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,18 +2,62 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Tag;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/** 
+ *  @Route("", name="question_") 
+*/
 class QuestionController extends AbstractController
 {
     /**
-     * @Route("/question", name="question")
+     * @Route("/", name="index", methods={"GET"})
      */
     public function index()
     {
         return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
+            'page_title' => 'Les questions des utilisateurs',
+        ]);
+    }
+
+    /**
+     * @Route("/question/tag/{id}", name="indexByTag", methods={"GET"})
+     */
+    public function indexByTag(Tag $tag)
+    {
+        return $this->render('question/index.html.twig', [
+            'page_title' => 'Catégorie - ' . $tag->getName(),
+        ]);
+    }
+
+    /**
+     * @Route("/question/new", name="new", methods={"GET", "POST"})
+     */
+    public function new()
+    {
+        return $this->render('question/index.html.twig', [
+            'page_title' => 'Ajouter une nouvelle question',
+        ]);
+    }
+
+    /**
+     * @Route("/question/{id}", name="show", methods={"GET"})
+     */
+    public function show(Question $question)
+    {
+        return $this->render('question/index.html.twig', [
+            'page_title' => 'Question - ' . $question->getTitle(),
+        ]);
+    }
+
+    /**
+     * @Route("/question/{id}", name="editStatus", methods={"PUT"})
+     */
+    public function editStatus(Question $question)
+    {
+        return $this->render('question/index.html.twig', [
+            'page_title' => 'Question - ' . $question->getTitle(),
         ]);
     }
 }
