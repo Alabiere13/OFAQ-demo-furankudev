96eee387a774b79d5e62f81f02a48d5008ed3dad
Live session user <furanku13code@gmail.com>
2019-03-10 21:55:12 +0100

    template show backend user et toggle Role entre user et moderator

--- src/Controller/Backend/UserController.php
+++ src/Controller/Backend/UserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Backend;
 
+use App\Entity\User;
+use App\Repository\RoleRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -22,5 +25,35 @@ class UserController extends AbstractController
             'page_title' => 'Administration - Liste des utilisateurs',
             'users' => $users
         ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(User $user)
+    {
+        return $this->render('backend/user/show.html.twig', [
+            'page_title' => 'Profil - ' . $user->getUsername(),
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/editRole", name="editRole", methods={"PATCH"})
+     */
+    public function editRole(User $user, RoleRepository $roleRepo, EntityManagerInterface $entityManager)
+    {
+        $moderatorRole = $roleRepo->findOneByName('Modérateur');
+        $userRole = $roleRepo->findOneByName('Utilisateur');
+
+        if ($user->getRole() == $userRole) {
+            $user->setRole($moderatorRole);
+        } elseif ($user->getRole()  == $moderatorRole) {
+            $user->setRole($userRole);
+        } 
+
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('backend_user_index');
     }
 }
